53d016e28056a02d7ee728cfab1fd3792f1345f3
Kostiantyn Teltov <kostiantyn.teltov@frontiersin.org>
2024-02-07 12:32:45 +0100

    POST /User API test was added

--- Dneprokos.RestApi.Tests/Tests/PetApiTestsBase.cs
+++ Dneprokos.RestApi.Tests/Tests/PetApiTestsBase.cs
@@ -1,19 +1,20 @@
-﻿using Dneprokos.RestApi.Framework.Requests.Pet;
+﻿using Dneprokos.RestApi.Framework.Requests;
 using NUnit.Framework;
 
 namespace Dneprokos.RestApi.Tests.Tests
 {
     [TestFixture]
+    [Parallelizable(ParallelScope.All)]
     public abstract class PetApiTestsBase
     {
-        protected PetApiRequests? PetApiRequests;
+        protected PetStoreApiFacade? PetApi;
         protected static string? BaseUrl;
 
         [OneTimeSetUp]
         public void BeforeFixture()
         {
             BaseUrl = GlobalSetup.BaseUrl;
-            PetApiRequests = new PetApiRequests(BaseUrl!);
+            PetApi = new PetStoreApiFacade(BaseUrl!);
         }
     }
 }
---
+++ Dneprokos.RestApi.Tests/Tests/User/PostUserApiTests.cs
@@ -0,0 +1,45 @@
+﻿using Dneprokos.RestApi.Framework.Models.Common;
+using Dneprokos.RestApi.Framework.Models.User;
+using Dneprokos.RestApi.Framework.Utils;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using NUnit.Framework;
+using RestSharp;
+using System.Net;
+
+namespace Dneprokos.RestApi.Tests.Tests.User
+{
+    public class PostUserApiTests : PetApiTestsBase
+    {
+        [Test]
+        public void CreateUser_WithValidFullData_ShouldBeCreated()
+        {
+            //Arrange
+            var userBody = new UserApiModelV2
+            {
+                Email = "test@test.com",
+                FirstName = "Kostia",
+                LastName = "Teltov",
+                Password = "123456",
+                Phone = "1234567890",
+                UserStatus = 1
+            };
+
+            //Act
+            RestResponse response = PetApi!.UserSection()
+                .ExecutePostUserRequest(userBody);
+
+            //Assert
+            response.StatusCode
+                .Should()
+                .Be(HttpStatusCode.OK);
+            CommonApiResponseModel responseBody = response
+                .ConvertToModel<CommonApiResponseModel>();
+            using (new AssertionScope())
+            {
+                responseBody.Code.Should().Be(200);
+                responseBody.Type.Should().Be("unknown");
+            }
+        }
+    }
+}
